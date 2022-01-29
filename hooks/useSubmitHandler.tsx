96355c46e6da3c98936e3db97aff14334e199006
aarenstade <aarenstade@gmail.com>
2022-01-28 21:18:05 -0800

    handle submission org / var changes

--- hooks/useSubmitHandler.tsx
+++ hooks/useSubmitHandler.tsx
@@ -1,7 +1,6 @@
-import { uploadImage } from "../utils/image-utils";
-import { createFullAdditionsImage, insertNewAddition, mergeNewCollage } from "../upload";
+import { convertBase64ToBytes, uploadImage } from "../utils/image-utils";
+import { insertNewAddition, mergeNewAddition } from "../upload";
 import { useState } from "react";
-import { useRecoilState } from "recoil";
 import { AdditionSubmitFormValues } from "../types/general";
 import { AdditionItem } from "../types/mongodb/schemas";
 import useAuth from "./useAuth";
@@ -32,41 +31,39 @@ const useSubmitHandler = (): SubmitHandlerHook => {
     return true;
   };
 
-  // TODO rename vars
-
   const handleSubmission = async (form: AdditionSubmitFormValues) => {
     try {
       if (auth?.firebase?.token) {
-        setMessage("Handling Images...");
-        const additionsImage = await createFullAdditionsImage();
-        if (!additionsImage) throw new Error("Failed to compile additions...");
+        const canvas = document.getElementById("canvas-layer-canvas") as HTMLCanvasElement | null;
+        if (canvas) {
+          setMessage("Preparing Drawing...");
 
-        setMessage("Merging Additions...");
-        const additionUrl = await uploadImage(additionsImage, `/tmp/${v4()}.png`);
-        if (!additionUrl) throw new Error("Additions failed to upload...");
+          const addition = canvas.toDataURL("image/png");
+          const additionUrl = await uploadImage(convertBase64ToBytes(addition), `/tmp/${v4()}.jpg`);
+          if (!additionUrl) throw new Error("Additions failed to upload...");
 
-        // embed new collage
-        const merge = await mergeNewCollage(additionUrl, drawing.addition?.url);
-        if (!merge?.storagePath) throw new Error("Error merging additions...");
+          setMessage("Merging Drawing...");
+          const merge = await mergeNewAddition(auth.firebase.token, additionUrl, drawing.addition?.url);
+          if (!merge?.storagePath) throw new Error("Error merging additions...");
 
-        setMessage("Finalizing...");
-        const newCollage = await getDownloadURL(STORAGE_REF(merge.storagePath));
-        let newAddition: AdditionItem = {
-          topic_id: drawing.topic?._id,
-          url: newCollage,
-          name: form.name,
-          email: form.email,
-          description: form.description,
-          address: auth.eth.account || undefined,
-          timestamp: new Date(),
-        };
+          setMessage("Finalizing...");
+          const newBase = await getDownloadURL(STORAGE_REF(merge.storagePath));
+          let newAddition: AdditionItem = {
+            topic_id: drawing.topic?._id,
+            url: newBase,
+            name: form.name,
+            email: form.email,
+            description: form.description,
+            address: auth.eth.account || undefined,
+            timestamp: new Date(),
+          };
+          const additionInsert = await insertNewAddition(auth.firebase.token, newAddition, drawing.topic);
+          if (!additionInsert._id) throw new Error("Addition failed to insert...");
 
-        const addition = await insertNewAddition(auth.firebase.token, newAddition, drawing.topic);
-        if (!addition._id) throw new Error("Addition failed to insert...");
-
-        setLiveImage(newCollage);
-        setSuccess(true);
-        setMessage("Success!");
+          setLiveImage(newBase);
+          setSuccess(true);
+          setMessage("Success!");
+        }
       }
     } catch (error) {
       if (typeof error === "string") setMessage(error);
