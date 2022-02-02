2e764f16b104c552ea97a281299b26c4f2e65490
aarenstade <aarenstade@gmail.com>
2022-02-01 18:56:39 -0800

    misc changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,15 +7,19 @@ const AboutPage: NextPage = () => {
       <div className={container.contentContainer}>
         <h3>About WikiDraw</h3>
         <p>
-          WikiDraw is a drawing program for open collaborations on drawings of any topic, like Wikipedia, but cooler.
+          WikiDraw is a drawing program for open collaborations on drawings of any topic, like Wikipedia, but cooler. 😎
         </p>
         <p>Anyone can explore topics and add drawings. Each topic starts with an empty 1000 x 1000 pixel canvas.</p>
       </div>
+      <div style={{ width: "80%", height: "1px", backgroundColor: "black" }} />
       <div className={container.contentContainer}>
         <p>
-          Created by <a href="https://twitter.com/AarenStade">Aaren Stade</a>
+          Created by{" "}
+          <a href="https://twitter.com/AarenStade" target="_blank">
+            Aaren Stade
+          </a>
         </p>
-        <p>Built with NextJS and MongoDB</p>
+
         <p>
           Any bugs or feature requests, email: <a href="aarenstade@gmail.com">aarenstade@gmail.com</a>
         </p>
