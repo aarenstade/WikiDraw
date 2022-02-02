b78f9c505b66787fd16305b1cfc160ccc2018456
aarenstade <aarenstade@gmail.com>
2022-02-01 18:56:30 -0800

    help button timing

--- components/menu/HelpButton.tsx
+++ components/menu/HelpButton.tsx
@@ -10,12 +10,12 @@ const HelpButton = () => {
   return (
     <div className={styles.helpButton}>
       {showDialog && (
-        <HelperDialog bottom="40px" right={0}>
+        <HelperDialog bottom="40px" right={0} displayTime={2500}>
           <h4>How to use:</h4>
           <ul style={{ fontSize: "14px", padding: "0", listStyleType: "none" }}>
             <li>1. Select color (left) and brush size (right).</li>
             <li>2. Draw a nice picture!</li>
-            <li>3. Click Submit Additions to upload!</li>
+            <li>3. Click "Submit Additions" to upload!</li>
           </ul>
         </HelperDialog>
       )}
